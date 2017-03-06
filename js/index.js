791f01dbed2a324b0fe8e5af0d3625a7f7802baf
Said Muhammad <saidmsaid81@gmail.com>
2017-03-07 01:41:58 +0300

    testing different method

--- js/index.js
+++ js/index.js
@@ -66,45 +66,6 @@ var morning =new Date()
         console.log('Received Event: ' + id);
     }
 };
-var admobid = {};
 
-// TODO: replace the following ad units with your own
-if( /(android)/i.test(navigator.userAgent) ) { 
-  admobid = { // for Android
-    banner: 'ca-app-pub-6981212130280607/8129501976',
-    interstitial: ''
-  };
-} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-  admobid = { // for iOS
-    banner: 'ca-app-pub-6981212130280607/8129501976',
-    interstitial: ''
-  };
-} else {
-  admobid = { // for Windows Phone
-    banner: 'ca-app-pub-6869992474017983/8878394753',
-    interstitial: ''
-  };
-}
-
-function initApp() {
-  if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
-
-  // this will create a banner on startup
-  AdMob.createBanner( {
-    adId: admobid.banner,
-    position: AdMob.AD_POSITION.BOTTOM_CENTER,
-    isTesting: true, // TODO: remove this line when release
-    overlap: true,
-    offsetTopBar: false,
-    bgColor: 'black'
-  } );
-
-  // this will load a full screen ad on startup
-  AdMob.prepareInterstitial({
-    adId: admobid.interstitial,
-    isTesting: true, // TODO: remove this line when release
-    autoShow: true
-  });
-}
 
 app.initialize();
