9c810459fee9f00b7e5a1f9997d7385bc360fdbb
Said Muhammad <saidmsaid81@gmail.com>
2017-04-25 13:42:23 +0300

    testing file reading

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.addEventListener('deviceready', this.onDeviceReady, false);
          AppRate.preferences.storeAppURL.android = 'market://details?id=com.ihyausunnah.surahalkahf';
         AppRate.preferences.openStoreInApp = true;
         AppRate.preferences.displayAppName = 'Surah Al Kahf App';
-        AppRate.preferences.usesUntilPrompt = 2;
+        AppRate.preferences.usesUntilPrompt = 10;
          AppRate.preferences.promptAgainForEachNewVersion = false;
          AppRate.promptForRating(false);
 
