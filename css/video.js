320b8ad3b2e4cacb007875114f2c5c6fb8ce9869
Said Muhammad <saidmsaid81@gmail.com>
2017-04-26 17:02:23 +0300

    test

--- css/video.js
+++ css/video.js
@@ -7,25 +7,8 @@ error: function()
     //file not exists
 function onConfirm(buttonIndex) {
   if (buttonIndex==1) {
-var downloadUrl = encodeURI(cordova.file.dataDirectory + fileName);
-var hostUrl = encodeURI("https://player.vimeo.com/play/159626069?s=64552532_1493212329_848f2ba4bab4943bb2a528d9ad04bc93&loc=external&context=Vimeo%5CController%5CClipController.main&download=1"); 
-var fileTransfer = new FileTransfer();
-fileTransfer.download(
-    hostUrl,
-    downloadUrl,
-    function(entry) {
-        alert('Your download has completed.');
-    },
-    function(error) {
-        alert(error.source);
-    },
-    false,
-    {
-        headers: {
-            "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
-        }
-    }
-);  } 
+fileTransfer.download()
+  } 
 else {
      playVideo(document.getElementById("vidUrlonline").value);
   }
@@ -34,8 +17,8 @@ else {
 navigator.notification.confirm(
     'Would you like to download the file so as its available offline next time or Just play without downloading?', // message
      onConfirm,            // callback to invoke with index of button pressed
-    'Download/Play(Experimental Feature)',           // title
-    ['Download','just play without downloading']     // buttonLabels
+    'Download/Play',           // title
+    ['Download','Play without downloading']     // buttonLabels
 );
 },
 success: function()
@@ -44,3 +27,19 @@ success: function()
 playVideo(document.getElementById("vidUrl").value);
 }
 });}
+
+       var fileTransfer = new FileTransfer();
+ var url = 'http://cordova.apache.org/static/img/cordova_bot.png';
+ var filePath ='file:///storage/emulated/0/Download/'+fileName 
+fileTransfer.download(
+    url,
+    filePath,
+    function(entry) {
+        console.log("download complete: " + entry.fullPath);
+    },
+    function(error) {
+        console.log("download error source " + error.source);
+        console.log("download error target " + error.target);
+        console.log("upload error code" + error.code);
+    }
+);
