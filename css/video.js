b449277054e9e2dc17a1cd369811dd950046d126
Said Muhammad <saidmsaid81@gmail.com>
2017-04-26 15:24:53 +0300

    test

--- css/video.js
+++ css/video.js
@@ -6,23 +6,13 @@ error: function()
 {
     //file not exists
 function onConfirm(buttonIndex) {
-  switch(buttonIndex) {
-    case 1:
+  if (buttonIndex==1) {
     window.open('https://player.vimeo.com/play/159626069?s=64552532_1493206655_14cb47caa3c50a5f7b774b336d18bc83&loc=external&context=Vimeo%5CController%5CClipController.main&download=1','_blank')
-  break;
-  case 2 :
-  window.open('https://player.vimeo.com/video/64552532')
-break;
-default:
-
+  } else {
+     window.open('https://player.vimeo.com/video/64552532')
   }
-  
     
-
-  } 
-    
-}}
-
+}
 navigator.notification.confirm(
     'Would you like to download the file so as its available offline next time or Just play without downloading?', // message
      onConfirm,            // callback to invoke with index of button pressed
