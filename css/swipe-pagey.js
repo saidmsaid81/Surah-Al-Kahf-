4a3074ef5a8c3d6adb2499916767d2544329b016
Said Muhammad <saidmsaid81@gmail.com>
2018-05-06 01:55:11 +0300

    Used to javascript to swipe between pages

--- css/swipe-pagey.js
+++ css/swipe-pagey.js
@@ -1,4 +1,8 @@
-$( document ).on( "pageinit", "[data-role='page'].nd2-no-menu-swipe", function() {
+
+
+$( document ).on( "pageinit", "[data-role='page'].demo-page", function() {
+	
+	
 	var page = "#" + $( this ).attr( "id" ),
 		// Get the filename of the next page that we stored in the data-next attribute
 		next = $( this ).jqmData( "next" ),
@@ -8,14 +12,14 @@ $( document ).on( "pageinit", "[data-role='page'].nd2-no-menu-swipe", function()
 	// Check if we did set the data-next attribute
 	if ( next ) {
 		// Prefetch the next page
-		$.mobile.loadPage( "pages.html"+ next );
+		$.mobile.loadPage( "one.html"+ next );
 		// Navigate to next page on swipe left
 		$( document ).on( "swipeleft", page, function() {
-			$.mobile.changePage( "pages.html"+ prev, { ajax: "false" });
+			$.mobile.changePage( "one.html"+ prev, { ajax: "false" });
 		});
 		// Navigate to next page when the "next" button is clicked
 		$( ".control .next", page ).on( "click", function() {
-			$.mobile.changePage( "pages.html"+ prev, { ajax: "false" } );
+			$.mobile.changePage( "one.html"+ prev, { ajax: "false" } );
 		});
 	}
 	// Disable the "next" button if there is no next page
@@ -25,13 +29,14 @@ $( document ).on( "pageinit", "[data-role='page'].nd2-no-menu-swipe", function()
 	// The same for the previous page (we set data-dom-cache="true" so there is no need to prefetch)
 	if ( prev ) {
 		$( document ).on( "swiperight", page, function() {
-			$.mobile.changePage( "pages.html"+next  , { ajax: "false", reverse: true } );
+			$.mobile.changePage( "one.html"+next  , { ajax: "false", reverse: true } );
 		});
 		$( ".control .prev", page ).on( "click", function() {
-			$.mobile.changePage( "pages.html" + next, { ajax: "false", reverse: true } );
+			$.mobile.changePage( "one.html" + next, { ajax: "false", reverse: true } );
 		});
 	}
 	else {
 		$( ".control .prev", page ).addClass( "ui-disabled" );
 	}
 });
+
