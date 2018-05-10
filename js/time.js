57eef85df3245cafe96f40ce71d9e413fa2011b8
Said Muhammad <saidmsaid81@gmail.com>
2018-05-11 01:43:52 +0300

    Try setting notification reminders according to users choice and removed settings tab in trans

--- js/time.js
+++ js/time.js
@@ -2,8 +2,8 @@ var friday = new Date();
 		friday.setDate(friday.getDate() + (1 + 4 - friday.getDay()) % 7);
 var morning =new Date()
 		morning.setDate(friday.getDate());
-		morning.setHours(8);
-		morning.setMinutes(0);
+		morning.setHours(localStorage.getItem("hourVal"));
+		morning.setMinutes(localStorage.getItem("minuteVal"));
 		morning.setSeconds(0);
 		var friday_in_morning = new Date(morning);
 
@@ -11,7 +11,7 @@ var morning =new Date()
         thursday.setDate(thursday.getDate() + (1 + 3 - thursday.getDay()) % 7);
 var night =new Date()
         night.setDate(thursday.getDate());
-        night.setHours(20);
-        night.setMinutes(0);
+        night.setHours(localStorage.getItem("thursHourVal");
+        night.setMinutes(localStorage.getItem("thursMinuteVal");
         night.setSeconds(0);
         var thursday_in_night = new Date(night);
