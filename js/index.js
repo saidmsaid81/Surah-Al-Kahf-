214468d3912ee912b18d61731860e5ea1e81a1aa
Said Muhammad <saidmsaid81@gmail.com>
2018-05-11 04:52:54 +0300

    fixed notification and ads

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+var admobid = {
+  banner: 'ca-app-pub-6981212130280607/8129501976',
+    interstitial: 'ca-app-pub-6981212130280607/9606235175',
+ };
+
 document.addEventListener('deviceready', this.onDeviceReady, false);
      function onDeviceReady() {
         welcomeScreen();
@@ -7,8 +12,14 @@ document.addEventListener('deviceready', this.onDeviceReady, false);
         AppRate.preferences.usesUntilPrompt = 10;
         AppRate.preferences.promptAgainForEachNewVersion = false;
         AppRate.promptForRating(false);
+        admob.banner.config({
+        id: admobid.banner,
+        isTesting: false,
+        autoShow: true,
+          })
+        admob.banner.prepare()
         AdMob.createBannerView();
-		AdMob.showAd(true);
+        AdMob.showAd(true);
 
 
         
