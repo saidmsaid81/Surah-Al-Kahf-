6804cdf7eead2a5f8e20e2babd1d6f23163a72f4
Said Muhammad <saidmsaid81@gmail.com>
2018-05-15 23:25:22 +0300

    Used version 0.8 of notification plugin

--- js/checkbox.js
+++ js/checkbox.js
@@ -6,14 +6,14 @@ $("#checkboxi").change(function() {
 
 else {
   $("#save-fri").prop("disabled",true);
+  localStorage.removeItem("hourVal");
+  localStorage.removeItem("minuteVal");
    cordova.plugins.notification.local.cancel(1, function() {
    window.plugins.toast.showLongTop("Successfully Disabled");
 });
 
  }
- cordova.plugins.notification.local.on("click", function (notification, state) {
-   admob.banner.hide(); window.open('pages.html');
-}, this)
+ 
 
 });
   $("#checkboxii").change(function() {
@@ -22,15 +22,14 @@ else {
   }
   else {
     $("#save-thurs").prop("disabled",true);
+    localStorage.removeItem("thursHourVal");
+  localStorage.removeItem("thursMinuteVal");
    cordova.plugins.notification.local.cancel(2, function() {
    window.plugins.toast.showLongTop("Successfully Disabled");
 });
  }
-  cordova.plugins.notification.local.on("click", function (notification, state) {
-        AdMob.hideBanner(); window.open('pages.html');
-        }, this);
+  
 });
-
 
   $("#checkbox-container :checkbox").on("change", function(){
   
@@ -48,28 +47,4 @@ $.each(checkboxValues, function(key, value) {
   $("#" + key).prop('checked', value);
 });
 
-function setNotificationFriday() {
-  cordova.plugins.notification.local.schedule({
-    id: 1,
-    title: 'Reminder to recite Suratul Kahf',
-    text: 'Tap here to read now',
-    firstAt: localStorage.getItem("friNotiftime"),
-    icon: 'res://icon',
-    smallIcon: 'res://ic_popup_reminder',
-    every: 10080 // 
-    });window.plugins.toast.showLongTop("Successfully Enabled");
-  
-}
 
-function setNotificationThurs() {
-  cordova.plugins.notification.local.schedule({
-    id: 2,
-    title: 'Reminder to recite Suratul Kahf',
-    text: 'Tap here to read now',
-    firstAt: localStorage.getItem("thursNotiftime"),
-    icon: 'res://icon',
-    smallIcon: 'res://ic_popup_reminder',
-    every: 10080 // 
-    });window.plugins.toast.showLongTop("Successfully Enabled");
-  
-}
--- js/time.js
+++ js/time.js
@@ -1,22 +1,47 @@
-function updateFridayTime() {
 var friday = new Date();
-		friday.setDate(friday.getDate() + (1 + 4 - friday.getDay()) % 7);
+friday.setDate(friday.getDate() + (1 + 4 - friday.getDay()) % 7);
 var morning =new Date();
-		morning.setDate(friday.getDate());
-		morning.setHours(localStorage.getItem("hourVal"));
-		morning.setMinutes(localStorage.getItem("minuteVal"));
-		morning.setSeconds(0);
-		var friday_in_morning = new Date(morning);
-		localStorage.setItem("friNotiftime", friday_in_morning);
+morning.setDate(friday.getDate());
+morning.setHours(localStorage.getItem("hourVal"));
+morning.setMinutes(localStorage.getItem("minuteVal"));
+morning.setSeconds(0);
+var friday_in_morning = new Date(morning);
+		
+
+
+var thursday = new Date();
+thursday.setDate(thursday.getDate() + (1 + 3 - thursday.getDay()) % 7);
+var night =new Date();
+night.setDate(thursday.getDate());
+night.setHours(localStorage.getItem("thursHourVal"));
+night.setMinutes(localStorage.getItem("thursMinuteVal"));
+night.setSeconds(0);
+var thursday_in_night = new Date(night);
+
+function setNotificationFriday() {
+cordova.plugins.notification.local.schedule({
+    id: 1,
+    title: 'Reminder to recite Suratul Kahf',
+    text: 'Tap here to read now',
+    firstAt: friday_in_morning,
+    every: 10080 // 
+    });window.plugins.toast.showLongTop('Successfully enabled');
+  
+ cordova.plugins.notification.local.on("click", function (notification, state) {
+   AdMob.hideBanner(); window.open('pages.html');
+}, this);
 }
-function updateThursTime() {	
-		var thursday = new Date();
-        thursday.setDate(thursday.getDate() + (1 + 3 - thursday.getDay()) % 7);
-		var night =new Date();
-        night.setDate(thursday.getDate());
-        night.setHours(localStorage.getItem("thursHourVal"));
-        night.setMinutes(localStorage.getItem("thursMinuteVal"));
-        night.setSeconds(0);
-        var thursday_in_night = new Date(night);
-        localStorage.setItem("thursNotiftime", thursday_in_night)
-    }
+        
+ function setNotificationThurs() {
+cordova.plugins.notification.local.schedule({
+    id: 2,
+    title: 'Reminder to recite Suratul Kahf',
+    text: 'Tap here to read now',
+      firstAt: thursday_in_night,
+    every: 10080 // 
+    });window.plugins.toast.showLongTop('Successfully enabled', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)})
+  }
+  
+ cordova.plugins.notification.local.on("click", function (notification, state) {
+   AdMob.hideBanner(); window.open('pages.html');
+}, this);
